1c8477909a64fb72e492ff3941c97fca4d40bfae
hamza-4li <sy.hamza.4li@gmail.com>
2024-08-20 23:33:57 +0500

    add routers and controllers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import dotenv from 'dotenv';
 import DBconnect from './db/db.js';
+import {app} from './app.js'
 
 dotenv.config({ path: './env' });
 
 DBconnect()
   .then(() => {
+    app.on('error', (error) => {
+      console.log('ERROR:', error);
+      throw error;
+    });
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running at PORT: ${process.env.PORT}`);
     });
